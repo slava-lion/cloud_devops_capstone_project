83b57db0d344108dbca6d517f89f84602bf4babe
slava-lion <slava.lion25@gmail.com>
2020-06-06 21:19:23 +0300

    break linter

--- Docker/app.py
+++ Docker/app.py
@@ -14,7 +14,7 @@ def scale(payload):
     """Scales Payload"""
     
     LOG.info(f"Scaling Payload: \n{payload}")
-    scaler = StandardScaler().fit(payload.astype(float))
+    scaler = StandardScaler().fiiiit(payload.astype(float))
     LOG.info(f"line 18 scaler : \n{scaler}")
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     LOG.info(f"line 20 scaled_adhoc_predict: \n{scaled_adhoc_predict}")
